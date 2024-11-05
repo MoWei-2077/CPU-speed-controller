3c2afe8672231d5fc9bcab86d469886c325070cc
XShePlus <a1960995065@163.com>
2024-11-05 22:03:48 +0800

    修改了CS-Controller,修改了自动编译脚本

--- build.py
+++ build.py
@@ -2,6 +2,33 @@ from time import time
 
 from subprocess import run
 
+from shutil import copy2
+from os import makedirs
+from os.path import join,basename
+from glob import glob
+# 源文件路径
+source_file = 'D:\AndroidStudio\AndroidProject\CSController\app\build\outputs\apk\release\CS-Controller*.apk'
+
+# 目标文件夹路径
+destination_folder = './magisk/'
+def moveF():
+    # 源文件夹路径
+    source_folder = "D:\\AndroidStudio\\AndroidProject\\CSController\\app\\build\\outputs\\apk\\release\\"
+
+    # 目标文件夹路径
+    destination_folder = './magisk/'
+
+    # 确保目标文件夹存在
+    makedirs(destination_folder, exist_ok=True)
+
+    # 使用通配符匹配文件
+    apk_files = glob(join(source_folder, 'CS-Controller*.apk'))
+
+    # 复制每个匹配的文件
+    for apk_file in apk_files:
+        destination_file = join(destination_folder, basename(apk_file))
+        copy2(apk_file, destination_file)
+
 def update(file_path, new_str , to_type):
     # 用于存储更新后的内容
     updated_lines = []
@@ -27,7 +54,8 @@ def update(file_path, new_str , to_type):
 file_path = './magisk/module.prop'  # 文件路径
 new_version_code = input("请输入要更改的版本号：")
 new_version_name = input("请输入要更改的版本名：")
-
+if input("请选择是否移入CSController(除了xshe谁也不要输入1)") == "1":
+    moveF()
 if new_version_code == "0" :
     new_version_code = round(time())
     print(f"时间戳版本号：{new_version_code}")
